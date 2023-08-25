e680344bd2a0868fa94694750261eefe224c4fae
Shyampatidar17 <shyampatidar171995@gmail.com>
2023-08-25 11:56:56 +0530

    just for check

--- logic-build/app.js
+++ logic-build/app.js
@@ -4,7 +4,7 @@ console.log("shyam")
 
 // Q1. Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
 
-// oneWay
+// oneWay to solve
 
 // function sum(num1,num2,num3,num4,num5){
 //     const plus = num1+num2+num3+num4+num5
