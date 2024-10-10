63da193e1bd8907e71a5762430edd689a047aed1
React JS <reactjs@advantal.net>
2024-10-10 18:03:07 +0530

    add new quetion

--- interview-javascript/app.js
+++ interview-javascript/app.js
@@ -3,23 +3,23 @@ console.log("hello world");
 
 // ============================================================
 
-function convertToNumber(str) {
-  // code here
+// function convertToNumber(str) {
+//   // code here
 
-  const remove = /[^$,]/gi;
-  const match = +str.match(remove).join("");
-  return match;
-}
+//   const remove = /[^$,]/gi;
+//   const match = +str.match(remove).join("");
+//   return match;
+// }
 
 // // expected answer 123123.63
 // console.log("convertToNumber ans:", convertToNumber("$123,123.63"));
 
 // ======================================================================
 
-function mergeTwoObjects(obj1, obj2) {
-  let obj = { ...obj1, ...obj2 };
-  return obj;
-}
+// function mergeTwoObjects(obj1, obj2) {
+//   let obj = { ...obj1, ...obj2 };
+//   return obj;
+// }
 
 // expected answer {a:1,b:2,c:3,d:4}
 // console.log(
@@ -114,4 +114,44 @@ function mergeTwoObjects(obj1, obj2) {
 //  }
 // const add = sum(10)
 // console.log(add(5))
+
+
+
+// // ============================================================
+
+// function convertToNumber(str) {
+//   // code here
+// }
+
+// // expected answer 123123.63
+// convertToNumber("$123,123.63");
+
+// // ======================================================================
+
+// function mergeTwoObjects(obj1, obj2) {
+//   // code here
+// }
+
+// // expected answer {a:1,b:2,c:3,d:4}
+//   mergeTwoObjects({ a: 1, b: 2 }, { c: 3, d: 4 })
+
+// // ======================================================================
+
+// function oddAndEven(arr) {
+//   // console.log("arr:", arr);
+//   // code here
+// }
+// // expected arr: [1,2,3,4,5,6,7,8,9,10]
+// // expected answer {even:[2, 4, 6, 8, 10],odd:[1, 3, 5, 7, 9]}
+// oddAndEven([...Array(10).keys()].map(() => parseInt(Math.random() * 10)))
+
+// //  ======================================================================
+
+// function pallindromeArray(arr) {
+//   // console.log("arr:", arr);
+//   // code here
+// }
+
+// // expected answer [true,false,true,false,true]
+// pallindromeArray([121, 123, 34543, 3212, 48984])
 
